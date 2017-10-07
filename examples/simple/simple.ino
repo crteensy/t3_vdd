fe6d2d9b8de085d771defd94a77a017ba41cd1a1
buffi <webmaster@avrbeginners.net>
2017-10-07 23:11:04 +0200

    update example

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -1,27 +1,13 @@
+#include <ADC.h>
 #include <t3_vdd.h>
-
-ADC* pAdc;
+ADC adc;
 
 void setup() {
-  pAdc = new ADC();
-  t3_Vdd vdd;
-  vdd.begin(pAdc);
-
-  elapsedMillis vddTimer;
-  const uint16_t vdd_update_period = 1000;
-  while(1)
-  {
-    if (vddTimer >= vdd_update_period)
-    {
-      vdd.update();
-      Serial.print("vdd = ");
-      Serial.print(vdd.get_mV());
-      Serial.println(" mv");
-      vddTimer = 0;
-    }
-  }
+  t3_vdd::enable_vref();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  uint16_t mv = t3_vdd::measure_vdd_mV(adc);
+  Serial.printf("vdd: %04d mV\n", mv);
+  delay(500);
 }
